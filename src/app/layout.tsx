aeeb55a2f72186dd261e28f842563e558982ca89
jeffersonjuni <jeftsantos76@gmail.com>
2026-02-10 21:43:58 -0300

    feat: estrutura base do projeto com app router criada

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Sistema de Controle de Chamados',
+  description: 'Aplicação corporativa de help desk',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
